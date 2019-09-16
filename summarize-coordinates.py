8fe6f446ff3ad214ab6e3c1429a338b27ca258d2
Anton Lebedevich <mabrek@gmail.com>
2019-09-16 23:56:18 +0300

    summarize coordinates experiment

---
+++ summarize-coordinates.py
@@ -0,0 +1,5 @@
+from utils import *
+
+summarize_to_chunks('experiments/good-best-coordinates.log.xz', 'experiments/good-best-coordinates-', 20, verbose=True)
+df = aggregate_chunks('experiments/good-best-coordinates-???.pkl')
+df.to_pickle('experiments/good-best-coordinates.pkl')
