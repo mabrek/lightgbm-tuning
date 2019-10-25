c1e8c3b4fc2a6b6b14e37d24a927af9fffcb4794
Anton Lebedevich <mabrek@gmail.com>
2019-10-26 01:12:08 +0300

    reduce chunk size

--- summarize-10krounds.py
+++ summarize-10krounds.py
@@ -2,6 +2,6 @@
 
 from utils import *
 
-summarize_to_chunks('experiments/wide-10krounds-5folds.log.xz', 'experiments/wide-10krounds-5folds-', 5, verbose=True)
+summarize_to_chunks('experiments/wide-10krounds-5folds.log.xz', 'experiments/wide-10krounds-5folds-', 5, chunksize=500, verbose=True)
 df = aggregate_chunks('experiments/wide-10krounds-5folds-???.pkl')
 df.to_pickle('experiments/wide-10krounds-5folds.pkl')
