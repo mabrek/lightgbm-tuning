ffe3af2dd5867875e9a4e25b5b7ea4dc4cc86062
Anton Lebedevich <mabrek@gmail.com>
2019-10-26 01:07:15 +0300

    summarize 10krounds experiment

---
+++ summarize-10krounds.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from utils import *
+
+summarize_to_chunks('experiments/wide-10krounds-5folds.log.xz', 'experiments/wide-10krounds-5folds-', 5, verbose=True)
+df = aggregate_chunks('experiments/wide-10krounds-5folds-???.pkl')
+df.to_pickle('experiments/wide-10krounds-5folds.pkl')
